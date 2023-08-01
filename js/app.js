dcf96ca1441aa1a3b74ab08cf34323e53f73e57b
Mateo <adrianricardo.rojas.contractor.cidentity@bbva.com>
2023-08-01 15:54:37 -0500

    Se agregan archivos html css y js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var boton = document.getElementById("t1");
+    boton.addEventListener('click', () => {
+      alert("La nueva informacion aun no esta lista")
+    });
+  });
